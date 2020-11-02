a99d2250dd9fcd2d9820485513e87383263b474d
reedjoe <reedjoe36@gmail.com>
2020-11-02 19:50:21 +0000

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout/Layout"
+
+import './styles.css';
+
+const Error404 = () => {
+  return (
+    <div>
+      <Layout>
+        <h2>404 Not Found</h2>
+      </Layout>
+    </div>
+  )
+}
+
+export default Error404;
