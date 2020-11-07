7e80a75c519bac09b353d42ef8c957eb07db0998
reedjoe <reedjoe36@gmail.com>
2020-11-07 20:17:06 +0000

    Update site colours

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,16 +6,16 @@ const Footer = class extends React.Component {
     render() {
         return (
             <footer>
-                <div class="footer-container">
+                <div className="footer-container">
                     <a href="https://github.com/reedjoe">
-                        <i class="fab fa-github footer-icon"></i>
+                        <i className="fab fa-github footer-icon"></i>
                     </a>
                     <a href="https://linkedin.com/in/joe-reed-developer">
-                        <i class="fab fa-linkedin footer-icon"></i>
+                        <i className="fab fa-linkedin footer-icon"></i>
                     </a>
                 </div>
-                <div class="footer-container">
-                    <p class="copyright">Joe Reed © <script>document.write( new Date().getFullYear() );</script>2020</p>
+                <div className="footer-container">
+                    <p className="copyright">Joe Reed © <script>document.write( new Date().getFullYear() );</script>2020</p>
                 </div>
 
             </footer>
